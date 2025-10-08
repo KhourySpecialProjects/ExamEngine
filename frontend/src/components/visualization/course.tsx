f098b13ff00c9b6907120d40b292fa0b1e57d8ac
MEO256 <Leonidbeer@Gmail.com>
2025-10-08 18:12:29 -0400

    design for course

---
+++ frontend/src/components/visualization/course.tsx
@@ -0,0 +1,29 @@
+import * as React from "react"
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+
+interface CourseProps {
+  color: string 
+  title: string
+  students: string
+  building: string
+  className?: string
+}
+
+export function Course({ color, title, students, building, className }: CourseProps) {
+    const stud_title = students + " students"
+    const bg_color = "bg-" + color + "-50"
+    const text_color = "text-" + color + "-500"
+    const border_color = "border-" + color + "-200"
+  return (
+    console.log(bg_color),
+    <Button
+      style={{ color: text_color }}
+      className={cn(bg_color, border_color, text_color, `${className} border-2 hover:bg-transparent hover:underline flex flex-col items-start gap-y-0 px-5 py-8`)}
+    >
+    <span className="font-bold">{title}</span>
+    <span className="text-xs">{stud_title}</span>
+    <span className="text-xs">{building}</span>
+    </Button>
+  )
+}

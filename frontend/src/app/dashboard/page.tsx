c0ae9674add2db6cc2c95827bc9879404ee17013
Khoi Ngo <billngo47@gmail.com>
2025-10-12 15:22:33 -0400

    feat: uploader component (#4)
    * feat: uploader components
    
    * feat: using Dialog instead of Card
    
    * feat: min width
    
    * feat: icon margin

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,3 +1,11 @@
-export default function Dashboard() {
-  return <>Hello dashboard</>;
+"use client";
+
+import { Uploader } from "@/components/upload/uploader";
+
+export default function UploadPage() {
+  return (
+    <div className="space-y-6 m-5">
+      <Uploader />
+    </div>
+  );
 }

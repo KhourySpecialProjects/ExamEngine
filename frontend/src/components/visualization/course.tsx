df05544f709d67308ed82b11b696dab3c5cf3614
Khoi Ngo <67892332+BillQK@users.noreply.github.com>
2025-10-23 23:31:13 -0400

    feat: compact view

--- frontend/src/components/visualization/course.tsx
+++ frontend/src/components/visualization/course.tsx
@@ -1,25 +1,42 @@
-import * as React from "react"
-import { Button } from "@/components/ui/button"
+import * as React from "react";
+import { Button } from "@/components/ui/button";
 
 interface CourseProps {
-  color: string 
-  title: string
-  students: string
-  building: string
-  className?: string
+  title: string;
+  students: string;
+  building: string;
+  className?: string;
 }
 
-export function Course({ color, title, students, building, className }: CourseProps) {
-    const stud_title = students + " students"
+const colors = ["red", "blue", "green"] as const;
+
+export function Course({ title, students, building, className }: CourseProps) {
+  const color = React.useMemo(() => {
+    return colors[Math.floor(Math.random() * colors.length)];
+  }, []);
+
+  const stud_title = `${students} students`;
+
   return (
     <Button
-      variant={color as "link" | "default" | "red" | "blue" | "yellow" | "green" | "ghost" | null | undefined}
+      variant={
+        color as
+          | "link"
+          | "default"
+          | "red"
+          | "blue"
+          | "yellow"
+          | "green"
+          | "ghost"
+          | null
+          | undefined
+      }
       size="sm"
-      className={`${className} border-2 hover:underline flex flex-col items-start gap-y-0 px-5 py-8`}
+      className={`${className} border-2 flex flex-col items-start gap-y-0 py-8 w-full no-underline`}
     >
-    <span className="font-bold">{title}</span>
-    <span className="text-xs">{stud_title}</span>
-    <span className="text-xs">{building}</span>
+      <span className="font-bold">{title}</span>
+      <span className="text-xs">{stud_title}</span>
+      <span className="text-xs">{building}</span>
     </Button>
-  )
+  );
 }

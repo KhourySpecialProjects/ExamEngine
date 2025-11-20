ffc45bdac6fe903e7e0106703f09d8599df9e58b
JoePortas05 <joeportas05@gmail.com>
2025-11-20 00:33:21 -0500

    Added http to allowed Dev Origins

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  allowedDevOrigins: ['3.16.218.153', '*.3.16.218.153'],
+  allowedDevOrigins: [
+   'http://3.16.218.153/',
+   'https://3.16.218.153/'
+ ]
 };
 
 export default nextConfig;

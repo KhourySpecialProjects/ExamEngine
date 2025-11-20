8b26fca7bd74bd0a8cbd160f3aa1974c8676b514
JoePortas05 <joeportas05@gmail.com>
2025-11-20 00:41:23 -0500

    Temporarily allowed for all origins in the backend while AWS Testing

--- backend/src/main.py
+++ backend/src/main.py
@@ -26,7 +26,7 @@ settings = get_settings()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.frontend_url, "http://127.0.0.1:3000"],
+    allow_origins=[settings.frontend_url, "http://127.0.0.1:3000", "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
